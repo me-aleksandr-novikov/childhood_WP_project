cc5e283fe7c2bd9f3f7d55f72d3c22bd29f7c7b3
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-10-23 20:12:00 +1000

    add_all

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+    add_action('wp_enqueue_scripts', 'childhood_scripts');
+
+    function childhood_scripts () {
+        wp_enqueue_style('childhood-style', get_stylesheet_uri());
+        // wp_enqueue_style('header-style', get_template_directory_uri() . '/assets/styles/main.min.css');
+        // wp_enqueue_style('animate-style', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
+
+
+        wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
+    };
+?>
