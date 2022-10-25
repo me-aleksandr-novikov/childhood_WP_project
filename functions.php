3ce0196f143e6bc9644b1deb72cadf92a6618fa8
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-10-26 00:05:36 +1000

    soft_toys_block_added

--- functions.php
+++ functions.php
@@ -10,4 +10,5 @@
         wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
     };
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 ?>
